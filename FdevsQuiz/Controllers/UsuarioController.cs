4e667dca97a286dfcc3d64f2f30af70084c7beb6
Felipe Evaristo <felipe.evaristo@UPPERCASE.LOCAL>
2021-05-08 01:08:20 -0300

    FirstCommit

---
+++ FdevsQuiz/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace FdevsQuiz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        public Usuario RecuperarUsuario() 
+        {
+            var jsonUsuario = System.IO.File.ReadAllText("Usuario.json");
+
+            var retornoUsuario = JsonConvert.DeserializeObject<Usuario>(jsonUsuario);
+
+            return retornoUsuario;
+        }
+    }
+}
---
+++ FdevsQuiz/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FdevsQuiz
+{
+    public class Usuario
+    {
+        public int CodigoUsuario { get; set; }
+        public string NomeUsuario { get; set; }
+        public string Email { get; set; }
+    }
+}
